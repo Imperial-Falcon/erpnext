733223c6c590819b06158e43bf81e97d5c352d3d
Leemon Chandra Prodeep <leemonchandra@gmail.com>
2026-03-03 01:38:49 +0600

    feat(b2b): update Home view navigation and features sections for improved layout and content

--- frontend/apps/b2b/src/views/Home.vue
+++ frontend/apps/b2b/src/views/Home.vue
@@ -16,11 +16,11 @@
 						1$ off for all products
 					</div>
 					<!-- Main nav bar -->
-					<nav class="text-sm text-gray-500 flex items-center gap-4 justify-center">
-						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Today's</a>
-						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Categories</a>
-						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Menufacturers</a>
-						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Top Offers</a>
+					<nav class="text-sm font-semibold text-gray-800 flex items-center gap-4 justify-center">
+						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Medicines</a>
+						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Surgical Products</a>
+						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Medical Devices</a>
+						<a href="#" class="hover:underline underline-offset-8 decoration-blue-500 decoration-2">Health Care</a>
 					</nav>
 					<!-- Search bar -->
 					<div class="font-medium rounded-sm border-2 border-blue-500 flex justify-between py-2 px-3 text-gray-500 items-center">
@@ -33,53 +33,64 @@
 					<div class="flex flex-row gap-1">
 						<div class="flex items-center gap-3 bg-white py-3 px-4 rounded-sm">
 							<div>
-								<ScrollText size="30"/>
+								<PackageSearch size="30" color="#0FA5E9"/>
 							</div>
 							<div class="text-wrap w-20">
-								<h3 class="text-xs text-gray-600">Request for Products</h3>
+								<h3 class="text-xs font-semibold text-gray-800">Source by Category</h3>
 							</div>
 						</div>
 						<div class="flex items-center gap-3 bg-white py-3 px-4 rounded-sm">
 							<div>
-								<Trophy size="30"/>
+								<ScrollText size="30" color="#0FA5E9"/>
 							</div>
 							<div class="text-wrap w-20">
-								<h3 class="text-xs text-gray-600">Top Ranking</h3>
+								<h3 class="text-xs font-semibold text-gray-800">Request for Products</h3>
 							</div>
 						</div>
 						<div class="flex items-center gap-3 bg-white py-3 px-4 rounded-sm">
 							<div>
-								<ScrollText size="30"/>
+								<Trophy size="30" color="#0FA5E9"/>
 							</div>
 							<div class="text-wrap w-20">
-								<h3 class="text-xs text-gray-600">Request for Quotation</h3>
+								<h3 class="text-xs font-semibold text-gray-800">Top Ranking</h3>
+							</div>
+						</div>
+						<div class="flex items-center gap-3 bg-white py-3 px-4 rounded-sm">
+							<div>
+								<ScrollText size="30" color="#0FA5E9"/>
+							</div>
+							<div class="text-wrap w-20">
+								<h3 class="text-xs font-semibold text-gray-800">Request for Quotation</h3>
 							</div>
 						</div>
 					</div>
 					<!-- Top customers section -->
-					<div class="grid grid-cols-2 divide-x justify-stretch">
-						<div>
-							<div class="flex items-center gap-3 flex-1">
+					<div class="grid grid-cols-2 divide-x justify-stretch bg-gradient-to-r from-red-500 to-orange-500 rounded-sm py-4">
+						<div class="flex justify-center items-center">
+							<div class="flex items-center gap-3 max-w-40">
 								<div>
-									<ScrollText size="30"/>
+									<TruckElectric size="30" color="#ffffff"/>
 								</div>
-								<div class="">
-									<h3 class="text-xs text-gray-600">Request for Products</h3>
+								<div class="text-wrap">
+									<h3 class="text-sm font-semibold text-white">Superfast!</h3>
+									<h6 class="text-xs font-light text-white/70">Same day delivery</h6>
 								</div>
 							</div>
 						</div>
-						<div>
-							<div class="flex items-center gap-3 flex-1">
+						<div class="flex justify-center items-center">
+							<div class="flex items-center gap-3 max-w-40">
 								<div>
-									<Trophy size="30"/>
+									<DiamondPercent size="30" color="#ffffff"/>
 								</div>
-								<div class="">
-									<h3 class="text-xs text-gray-600">Top Ranking</h3>
+								<div class="text-wrap">
+									<h3 class="text-sm font-semibold text-white">More Discount!</h3>
+									<h6 class="text-xs font-light text-white/70">Buy more get more</h6>
 								</div>
 							</div>
 						</div>
 					</div>
 				</section>
+				<!-- Product section -->
 				<!--product thumb  -->
 				<div class="bg-gray-100 flex items-center justify-center min-h-screen">
 					<!-- Card -->
@@ -134,5 +145,5 @@
 
 <script setup>
 import BaseLayout from "@/components/layouts/BaseLayout.vue"
-import { Search, ScrollText, Trophy } from 'lucide-vue-next';
+import { Search, ScrollText, Trophy, TruckElectric, PackageSearch, DiamondPercent } from 'lucide-vue-next';
 </script>
